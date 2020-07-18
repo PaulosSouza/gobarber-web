7b2d9faa53406d73ccc7a4e907cb0fc043ec6b7c
Paulo Henrique <phferreirasouza106@gmail.com>
2020-07-18 00:59:37 -0300

    feat: add auth context

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import { AuthProvider } from './context/AuthContext';
+
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
       <GlobalStyle />
     </>
   );
