6ffcc8d54f69fd1c46493ceb8e599aafe65a121f
Paulo Henrique <phferreirasouza106@gmail.com>
2020-07-22 20:03:05 -0300

    feat: refactor toasts and add new context

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import { AuthProvider } from './hooks/AuthContext';
-
 import GlobalStyle from './styles/global';
+
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthProvider>
+      <AppProvider>
         <SignIn />
-      </AuthProvider>
+      </AppProvider>
+
       <GlobalStyle />
     </>
   );
