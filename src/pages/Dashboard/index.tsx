688288c8f2dca7fef711203688608755572b0c32
Paulo Henrique <phferreirasouza106@gmail.com>
2020-10-23 13:39:37 -0300

    doing: Criando Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { Container, Profile, Header, HeaderContent } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="" />
+
+          <Profile>
+            <img
+              src="https://avatars0.githubusercontent.com/u/40366139?s=460&u=7f05ec541ae27c49db6df10ccf75cd805e45823d&v=4"
+              alt="Paulo Henrique"
+            />
+
+            <div>
+              <span>Bem vindo,</span>
+              <strong>Paulo Henrique</strong>
+            </div>
+          </Profile>
+
+          <button type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
